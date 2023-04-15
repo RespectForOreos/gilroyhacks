7aa519b712eeac05627834b18f3c4bb75ee282d5
DamonHuynh <130857766+DamonHuynh@users.noreply.github.com>
2023-04-15 14:47:10 -0700

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,25 +1,37 @@
+import java.util.ArrayList;
+
+
 public class Food {
-  
   private String name;
   private String [] ingredientList;
   private String recipe;
   
-  public Food(String name, String[] ingredientList, String recipe) { 
-    //takes in food name, recipe, and ingredients to construct a Food object
+  public Food(String name, String[] ingredientList, String recipe) //takes in food name, recipe, and ingredients
+  { 
     this.name = name;
     this.ingredientList = ingredientList;
     this.recipe = recipe;
   }
 
-  public String[] getIngredients() {
-    //returns ingredients
+  public String[] getIngredients ()
+  //returns ingredients
+  {
     return ingredientList;
+
+    
+  }
+   public String getRecipe ()
+  //returns recipes
+  {
+    return recipe;
+
+    
   }
   
-  public String[] getRecipe() {
-    //returns recipes
-    return recipe;
+  public String getName(){
+
+    return name;
+    
   }
   
 }
-
